5f6d0942f52d7481cb70fe764049f8e189eebda1
Antonio Gottsfritz <antoniobgott@gmail.com>
2020-08-16 16:21:31 -0300

    added CustomerController

---
+++ Server/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
