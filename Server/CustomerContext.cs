d829d0b099c421c5a135948326e10269e9d7c690
Antonio Gottsfritz <antoniobgott@gmail.com>
2020-08-19 21:46:39 -0300

    db controller generated

--- Server/CustomerContext.cs
+++ Server/CustomerContext.cs
@@ -5,11 +5,15 @@ namespace Server
 {
     public class CustomerContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=customers.db");
+
+        public CustomerContext(DbContextOptions<CustomerContext> options) : base(options)
+        {
+        }
+
         public DbSet<Person> People { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Dependent> Dependents { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-         => options.UseSqlite("Data Source=customers.db");
     }
 }
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace Server
 {
@@ -25,12 +19,21 @@ namespace Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CustomerContext>(opt
+                => opt.UseSqlite("Data Source=customers.db"));
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<CustomerContext>();
+                context.Database.EnsureCreated();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
