f57ff4ff6d840b85326f4dec055e9e6f5c0175eb
Antonio Gottsfritz <antoniobgott@gmail.com>
2020-08-21 04:38:54 -0300

    customer list

--- AngularClient/src/app/app.component.ts
+++ AngularClient/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularClient';
+export class AppComponent implements OnInit {
+    customers = [];
+    currentIndex = -1;
+
+    constructor(private customerService: CustomerService) { }
+
+    ngOnInit() {
+        this.getCustomers();
+    }
+
+    getCustomers() {
+        this.customerService.getList()
+            .subscribe(
+                data => {
+                    this.customers = data;
+                },
+                error => {
+                    console.error(error);
+                });
+    }
+
+    expandCustomer(customer, i) {
+        this.currentIndex = (this.currentIndex == i) ? -1 : i;
+    }
+
+    editCustomer(customer) {
+
+    }
 }
--- AngularClient/src/app/services/customer.service.ts
+++ AngularClient/src/app/services/customer.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:5000/api/customer';
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class CustomerService {
 
-  constructor() { }
+export class CustomerService {
+    constructor(private http: HttpClient) { }
+
+    getList(): Observable<any> {
+        return this.http.get(baseUrl);
+    }
+
+    get(id): Observable<any> {
+        return this.http.get(`${baseUrl}/${id}`);
+    }
+
+    create(data): Observable<any> {
+        return this.http.post(baseUrl, data);
+    }
+
+    update(id, data): Observable<any> {
+        return this.http.put(`${baseUrl}/${id}`, data);
+    }
+
+    delete(id): Observable<any> {
+        return this.http.delete(`${baseUrl}/${id}`);
+    }
 }
