75cf16dcddb4fe8eebc2ed9b1fb4db570c598177
Antonio Gottsfritz <antoniobgott@gmail.com>
2020-08-21 10:13:13 -0300

    separate customer list into component

--- AngularClient/src/app/app.component.ts
+++ AngularClient/src/app/app.component.ts
@@ -1,37 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { CustomerService } from 'src/app/services/customer.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-    customers = [];
-    currentIndex = -1;
-
-    constructor(private customerService: CustomerService) { }
-
-    ngOnInit() {
-        this.getCustomers();
-    }
-
-    getCustomers() {
-        this.customerService.getList()
-            .subscribe(
-                data => {
-                    this.customers = data;
-                },
-                error => {
-                    console.error(error);
-                });
-    }
-
-    expandCustomer(customer, i) {
-        this.currentIndex = (this.currentIndex == i) ? -1 : i;
-    }
-
-    editCustomer(customer) {
-
-    }
+export class AppComponent {
+    constructor() { }
 }
